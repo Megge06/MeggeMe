587da7333208ddd500d2c6a21ca952efade3e29f
Megge06 <maximmegge@gmail.com>
2026-07-15 20:26:01 +0200

    Small tweaks

--- frontend/about_me/script.js
+++ frontend/about_me/script.js
@@ -11,17 +11,18 @@ const dialogues = {
     `Currently I am studying computer science in a cooperative study program with a company. I won't tell you which company though, but maybe you can guess it. I challenge you! [smug]`,
     `I think my hobbies are quite clear from this website design. I like video games, programming and all sorts of other nerdy stuff. [selfassured]`,
     `If I had to give you some media that I like to give you an image of myself: [thinking]`,
-    `Games: Persona, Hearts of Iron, Red Dead Redemption 2, Minecraft [normal]`,
-    `Anime: JoJo's, Chainsaw Man, Attack On Titan, Saiki K, Bocchi, Mushoku Tensei [happy]`,
+    `Games: Persona, Hearts of Iron, Red Dead Redemption 2, Phoenix Wright, Genshin Impact [normal]`,
+    `Anime: JoJo's, Chainsaw Man, Saiki K, Bocchi, Mushoku Tensei [happy]`,
     `And a lot of other stuff. You can just ask me about anything if you want to know more. [normal]`,
   ],
   test2: [
     `Oho you wanna know about this website? [smug]`,
     `If you want to know technical stuff you can always look at my GitHub, it's open source. [normal]`,
     `But this is the first website I've ever made so expect some jankiness. But I am quite happy with how it looks. [happy]`,
-    `The pages are inspired by "Persona", "Milk outside a bag of milk outside a bag of milk", "Needy Streamer Overload", the Wii Menu,, Minecraft and other stuff that I haven't added yet so we'll see what happens. [thinking]`,
+    `The pages are inspired by Persona, "Milk outside a bag of milk outside a bag of milk", "Needy Streamer Overload", the Wii Menu,, Minecraft. [thinking]`,
     `I apologize to anyone using this site on the phone, it's not the best experience. But hey, it works and that's more than a lot of other IndieWeb sites can say. [selfassured]`,
     `The static sites are really just vanilla HTML, CSS and JS. No fancy frameworks here. But it's my first website so what did you expect. The only non vanilla stuff I used was Eleventy for my Blog and Photos page. [normal]`,
+    `Recently I have been working on a backend written in Golang for the Blog and Guestbook page. [thinking]`,
     `If you have any suggestions or want to help me improve the site, feel free to reach out! [happy]`,
     `As this is such a simple website though, I do not have much more to say about it. [dissatisfied]`,
     `You can just take a look around and see for yourself! [smug]`,
@@ -29,10 +30,10 @@ const dialogues = {
   test3: [
     `Right now hmmm. I'm afraid I won't update this section often enough, but here is what I am doing right NOW. [thinking]`,
     `I just started studying and working so a lot of my time is going into that. A lot of new experiences and sometimes it's exhausting. [tired]`,
-    `But I am still keeping up on my hobbies. I am currently playing the Phoenix Wright Trilogy, Hearts of Iron IV and Persona 3 Reload. I also still dabble with my GameCube and PSP. [happy]`,
+    `But I am still keeping up on my hobbies. I am currently playing the Phoenix Wright Trilogy, Genshin Impact and replaying Persona 5 Royal. I also still dabble with my GameCube and PSP. [happy]`,
     `In terms of anime, I am currently rewatching a bunch of stuff, whatever I want at the moment. [normal]`,
     `I am still watching the seasonal shows of course but I am also trying to catch up with Re:Zero and finish watching SNAFU. [normal]`,
-    `Of course I am currently programming this website and that is really my only private project right now. I don't want to start anything new before I finish this. I just got a Raspberry Pi to self host my website, but I have no experience with that yet. [thinking]`,
+    `Of course I am currently programming this website but I also try to do other stuff in my free time like learning more Golang and Linux desktops. (I use Arch and Hyprland btw) [thinking]`,
     `And that's pretty much what I'm doing right now. Of course I also spend time with friends and family and such, but that is not something relevant. [selfassured]`,
   ],
   test4: [
@@ -41,8 +42,8 @@ const dialogues = {
     `I'll try to focus on more "unknown" (all of it is still popular media but still) stuff I think more people should know about. [thinking]`,
     `Something I always wanna recommend is stuff I'm watching on YouTube. I can wholeheartedly recommend NeverKnowsBest and Moon Channel (Not "Moon", it's "Moon Channel". "Moon" is a bad YouTuber). [happy]`,
     `For Games of course any that I already mentioned in my About Me like Persona (any game in the series), but also Phoenix Wright, Hearts of Iron IV, Outer Wilds, and many others which I may add to this list. [normal]`,
-    `For more "unknown" anime I can recommend Monogatari, Daily lives of High School Boys, Saiki k, A Place Further than The Universe, Bocchi, Look Back and Grand Blue. [happy]`,
-    `These recommendations already feel fake and mainstream, not good. Take a look at my accounts as I said, you can get a bigger picture there. [paranoid]`,
+    `For more "unknown" anime I can recommend Mushoku Tensei, Saiki k, A Place Further than The Universe, Bocchi, Look Back and Grand Blue. [happy]`,
+    `These recommendations already feel too mainstream, not good. Take a look at my accounts as I said, you can get a bigger picture there. [paranoid]`,
     `The things I mentioned are still good though! [mentallyabsent]`,
   ],
 };
@@ -67,11 +68,18 @@ const characterImg = document.querySelector(".character");
 
 let index = 0;
 let isTyping = false;
+let typingTimeout;
+let currentCleanText = "";
 
 typewriterEffect(text[index]);
 
 function handleDialogueClick() {
-  if (isTyping) return;
+  if (isTyping) {
+    clearTimeout(typingTimeout);
+    dialogueText.textContent = currentCleanText;
+    isTyping = false;
+    return;
+  }
 
   if (index + 1 < text.length) {
     index++;
@@ -80,7 +88,6 @@ function handleDialogueClick() {
     options();
   }
 }
-
 dialogueBox.addEventListener("click", handleDialogueClick);
 dialogueText.addEventListener("click", handleDialogueClick);
 
@@ -100,24 +107,26 @@ function typewriterEffect(fullText) {
   changeImage(fullText);
 
   const cleanText = fullText.replace(/\[.*?\]/, "");
+  currentCleanText = cleanText;
 
   dialogueText.textContent = "";
 
   let charIndex = 0;
   const typingSpeed = 20;
 
+  clearTimeout(typingTimeout);
+
   function typeNextChar() {
     if (charIndex < cleanText.length) {
       dialogueText.textContent += cleanText.charAt(charIndex);
       charIndex++;
-      setTimeout(typeNextChar, typingSpeed);
+      typingTimeout = setTimeout(typeNextChar, typingSpeed);
     } else {
       isTyping = false;
     }
   }
   typeNextChar();
 }
-
 function options() {
   characterImg.src = "../assets/about_me/normal.webp";
   typewriterEffect("What would you like to talk about?");
